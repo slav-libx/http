708a9a069424e03cdab6b46cbb5c301294231bdc
slav-libx <my_990@bk.ru>
2019-02-22 00:49:24 +0300

    1

--- HTTPClient/httpclient.dpr
+++ HTTPClient/httpclient.dpr
@@ -2,8 +2,6 @@ program httpclient;
 
 uses
   Vcl.Forms,
-  Lib.JSON.Store in '..\lib\Lib.JSON.Store.pas',
-  Lib.JSON.Format in '..\lib\Lib.JSON.Format.pas',
   Lib.HeaderValues in 'Units\Lib.HeaderValues.pas',
   Form.ClientMain in 'Forms\Form.ClientMain.pas' {Form2},
   Form.Request in 'Forms\Form.Request.pas' {RequestForm};
